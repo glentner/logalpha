207182f1f40d3885a6de95dc2b657641f55aa6c3
Geoffrey Lentner <glentner@purdue.edu>
2019-05-19 23:44:06 -0400

    version 2.0.2

--- logalpha/__meta__.py
+++ logalpha/__meta__.py
@@ -11,7 +11,7 @@
 """Meta data for the LogAlpha package."""
 
 __appname__   = 'logalpha'
-__version__   = '2.0.1'
+__version__   = '2.0.2'
 __authors__   = 'Geoffrey Lentner'
 __contact__   = '<glentner@gmail.com>'
 __license__   = 'Apache Software License'
