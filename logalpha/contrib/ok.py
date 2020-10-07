71d146295dd08b2984567cfb238c5fa0348fe499
Geoffrey Lentner <glentner@purdue.edu>
2020-10-07 10:25:09 -0400

    Add contrib sub-package with recipes

---
+++ logalpha/contrib/ok.py
@@ -0,0 +1,61 @@
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the Apache License (v2.0) as published by the Apache Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the Apache License for more details.
+#
+# You should have received a copy of the Apache License along with this program.
+# If not, see <https://www.apache.org/licenses/LICENSE-2.0>.
+
+
+"""Simple colorized Ok/Err logging setup."""
+
+# type annotations
+from __future__ import annotations
+
+# standard libs
+from dataclasses import dataclass
+
+# internal libs
+from logalpha.color import Color, ANSI_RESET
+from logalpha.level import Level
+from logalpha.message import Message
+from logalpha.handler import StreamHandler
+from logalpha.logger import Logger
+
+
+LEVELS = Level.from_names(['Ok', 'Err'])
+COLORS = Color.from_names(['green', 'red'])
+OK = LEVELS[0]
+ERR = LEVELS[1]
+
+
+@dataclass
+class OkayHandler(StreamHandler):
+    """
+    A standard message handler writes to <stderr> by default.
+    Message format includes the level and the text.
+    """
+
+    def format(self, message: Message) -> str:
+        """Format the message."""
+        color = COLORS[message.level.value].foreground
+        return f'{color}{message.level.name:<3}{ANSI_RESET} {message.content}'
+
+
+class OkayLogger(Logger):
+    """Logger with StandardMessage and StandardHandler."""
+
+    levels = LEVELS
+    colors = COLORS
+
+    @classmethod
+    def with_level(cls, level: Level) -> OkayLogger:
+        """Setup a handler and register it with a new logger."""
+        if level in [OK, ERR]:
+            log = cls()
+            log.handlers.append(OkayHandler(level=level))
+            return log
+        else:
+            raise ValueError(f'level should be one of {LEVELS}')
---
+++ logalpha/contrib/standard.py
@@ -0,0 +1,70 @@
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the Apache License (v2.0) as published by the Apache Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the Apache License for more details.
+#
+# You should have received a copy of the Apache License along with this program.
+# If not, see <https://www.apache.org/licenses/LICENSE-2.0>.
+
+
+"""Standard logging setup."""
+
+# type annotations
+from typing import Dict, Type
+
+# standard libs
+from datetime import datetime
+from dataclasses import dataclass
+from socket import gethostname
+
+# internal libs
+from logalpha.level import Level
+from logalpha.message import Message
+from logalpha.handler import StreamHandler
+from logalpha.logger import Logger, CallbackMethod
+
+
+@dataclass
+class StandardMessage(Message):
+    """A message with standard attributes."""
+    level: Level
+    content: str
+    timestamp: datetime
+    topic: str
+    host: str
+
+
+class StandardHandler(StreamHandler):
+    """
+    A standard message handler writes to <stderr> by default.
+    Message format includes all attributes.
+    """
+
+    def format(self, message: StandardMessage) -> str:
+        """Format the message."""
+        ts = message.timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        return f'{ts} {message.host} {message.level.name:<8} [{message.topic}] {message.content}'
+
+
+# global constant for hostname
+HOST: str = gethostname()
+
+
+class StandardLogger(Logger):
+    """Logger with StandardMessage and StandardHandler."""
+
+    Message: Type[Message] = StandardMessage
+    callbacks: Dict[str, CallbackMethod] = {
+        'timestamp': datetime.now,
+        'host': (lambda: HOST)
+    }
+
+    topic: str = None
+
+    def __init__(self, topic: str) -> None:
+        """Initialize with `topic`."""
+        super().__init__()
+        self.topic = topic
+        self.callbacks['topic'] = (lambda: topic)
